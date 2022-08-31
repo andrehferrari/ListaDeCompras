dfe51eab1e4b76bd7c8180c0dbf7e4021d404820
André Ferrari <andrhenrique6488@gmail.com>
2022-08-31 12:09:56 -0300

    Finalizando a exclusão de itens e validação de formulário

--- js/jquery.js
+++ js/jquery.js
@@ -1,32 +1,66 @@
+  //  Variáveis dos elementos da página
+   var msgSucesso = $('#alert-cadastro-tarefa-sucesso')
+   var msgErro = $('#alert-cadastro-tarefa-erro')
+  
+  //  Função que cria o item na tabela
    $('#btn-save').click(function(){
+    var itemInput = $('#item')
+    var qntInput = $('#quantidade')
+    var precoInput = $('#preco')
+
+    // Criando os elementos que irão na tabela
     var linha = $('<tr/>')
     linha.addClass('linha-tarefa')
     var colunaItem = $('<td/>')
     var colunaQnt = $('<td/>')
     var colunaPreco = $('<td/>')
+    colunaPreco.addClass('preco-item')
     var tdButton = $('<td/>')
     var btnExcluir = $('<button/>')
+
+    // Adicionando as propriedades do botão excluir
+    btnExcluir.html('Excluir')
+    btnExcluir.addClass('btn btn-dark')
     btnExcluir.addClass('exclude')
     btnExcluir.click((e) => {
       e.target.parentNode.parentNode.remove()
     })
 
-    colunaItem.html($('#item').val())
-    colunaQnt.html($('#quantidade').val())
-    colunaPreco.html($('#preco').val())
-    btnExcluir.html('Excluir')
-    btnExcluir.addClass('btn btn-dark')
+    // Validando o formulário
+    if(!itemInput || !qntInput || !precoInput) {
+      msgSucesso.hide()
+      msgErro.show()
+      return false;
+    }
+    else {
+      msgErro.hide()
+      msgSucesso.show()
+      setTimeout(() => {
+        msgSucesso.hide()
+      }, 3000)
 
-    tdButton.append(btnExcluir)
-    linha.append(colunaItem)
-    linha.append(colunaQnt)
-    linha.append(colunaPreco)
-    linha.append(tdButton)
+      // Colocando os textos dos inputs nos elementos
+      colunaItem.html(itemInput.val())
+      colunaQnt.html(qntInput.val())
+      colunaPreco.html(precoInput.val())
+      
+      // Zerando os valores dos inputs
+      itemInput.val('')
+      qntInput.val('')
+      precoInput.val('')
 
-    $('#tabela-compras').append(linha)
-    return false;
+      // Inserindo os elementos na tabela
+      tdButton.append(btnExcluir)
+      linha.append(colunaItem)
+      linha.append(colunaQnt)
+      linha.append(colunaPreco)
+      linha.append(tdButton)
+      $('#tabela-compras').append(linha)
+      return false;
+    }
    });
 
+  //  Máscara de valor para o campo de preço
    $(function(){
       $('#preco').maskMoney({
         prefix:'R$ ',
